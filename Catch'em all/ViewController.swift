7ce42f2a64cb7f640b56caeda008d37dfe510fd3
solohubivan <solohubivan@gmail.com>
2024-05-30 09:20:01 +0300

    Initial Commit

---
+++ Catch'em all/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Catch'em all
+//
+//  Created by Ivan Solohub on 30.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
